56e21a6023da931c258729f0ac47bd38b50a4137
Bobbasrinivasareddy834 <srinivasareddybobba208@gmail.com>
2025-04-27 11:45:56 +0530

    first commit

--- app/mboard/page.js
+++ app/mboard/page.js
@@ -63,7 +63,6 @@ export default function Mboard() {
         fetchMainData();
     }, []);
     // console.log(totalExpense, "bu")
-    // console.log(totalIncome, "bu")
 
     useEffect(() => {
         if (!loading && (totalBudget || totalIncome || totalExpense)) {
