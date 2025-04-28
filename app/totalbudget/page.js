f8e840757d32d307f929403ecd7dedf7eb0917d3
SrinivasReddyBobba <gitcodingfiles@gmail.com>
2025-04-28 15:39:49 +0530

    seven

--- app/totalbudget/page.js
+++ app/totalbudget/page.js
@@ -66,7 +66,7 @@ export default function Employee() {
             });
 
             if (response.ok) {
-
+                alert('Added  successfully!');
                 const offcanvasElement = document.getElementById('offcanvasExampleuser');
                 const offcanvasInstance = bootstrap.Offcanvas.getInstance(offcanvasElement);
                 offcanvasInstance.hide();
